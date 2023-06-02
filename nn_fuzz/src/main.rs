8124dacbe7a18b6a7b27d813a3c93f1f16cf397d
psi_foo <shalaginmx998@yandex.ru>
2023-06-02 18:11:58 +0300

    + add logger

--- nn_fuzz/src/main.rs
+++ nn_fuzz/src/main.rs
@@ -4,7 +4,11 @@ static GLOBAL: MiMalloc = MiMalloc;
 
 use nn_fuzz::error::Error;
 
-fn main() {
+use env_logger::{init_from_env, Env};
+
+fn main() { 
+    let env = Env::new().filter("FUZZ_LOG").write_style("FUZZ_LOG_STYLE");
+    env_logger::init_from_env(env);
     let opts = nn_fuzz::cli::parse_args();
     match nn_fuzz::fuzz::fuzz(&opts) {
         Ok(_) | Err(Error::ShuttingDown) => {
